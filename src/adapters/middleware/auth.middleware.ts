041cbcdfab373737f62cddb0464b27da237003db
shellob <smakgames.studio@gmail.com>
2025-02-04 12:42:32 +0300

    fix auth middleware

--- src/adapters/middleware/auth.middleware.ts
+++ src/adapters/middleware/auth.middleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import * as jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-
+import { KafkaService } from "../kafka/kafka.service";
 dotenv.config();
 
 const SECRET_KEY = process.env.SECRET_KEY || "default_secret";
@@ -11,22 +11,36 @@ declare module "express" {
         user?: any;
     }
 }
+
+const kafkaService = new KafkaService();
+kafkaService.connect();
 // Middleware для проверки токена
-export function authenticateJWT(req: Request, res: Response, next: NextFunction): void {
+export async function authenticateJWT(req: Request, res: Response, next: NextFunction) {
     const authHeader = req.header("Authorization");
 
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-        res.status(401).json({ message: "Unauthorized: No token provided" });
-        return
+        return res.status(401).json({ message: "Unauthorized: No token provided" });
     }
+
     const token = authHeader.split(" ")[1];
 
     try {
-        const decoded = jwt.verify(token, SECRET_KEY);
-        req.user = decoded; // Добавляем пользователя в req
-        next(); // Передаём управление дальше
+        console.log("🔹 Отправляем токен в Kafka:", token);
+        await kafkaService.sendMessage("jwt-validation", { token });
+
+        // Ожидаем ответа от UserService
+        await kafkaService.consumeMessages("jwt-validation-response", async (message) => {
+            console.log("📩 Получили ответ от UserService:", message);
+            
+            if (!message.isValid) {
+                return res.status(403).json({ message: "Forbidden: Invalid token" });
+            }
+
+            req.user = message.decoded; // Добавляем пользователя в req
+            next(); // Передаём управление дальше
+        });
+
     } catch (error) {
-        res.status(403).json({ message: "Forbidden: Invalid token" });
-        return
+        return res.status(500).json({ message: "Internal server error" });
     }
 }
